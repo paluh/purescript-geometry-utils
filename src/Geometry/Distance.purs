510051ab35617f8caf4c78799301f2d38c9fe854
Tomasz Rybarczyk <paluho@gmail.com>
2019-03-13 09:35:08 +0100

    Add `Ord` instance for `Distance`

--- src/Geometry/Distance.purs
+++ src/Geometry/Distance.purs
@@ -8,6 +8,7 @@ foreign import kind SpaceUnit
 
 newtype Distance (unit ∷ SpaceUnit) = Distance Number
 derive instance eqDistance ∷ Eq (Distance u)
+derive instance ordDistance ∷ Ord (Distance u)
 derive instance genericDistance ∷ Generic (Distance u) _
 derive instance newtypeDistance ∷ Newtype (Distance u) _
 
