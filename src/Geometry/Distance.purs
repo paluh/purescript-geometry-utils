6045e569eeebc7a146cdf14a4e21154f25e80331
Tomasz Rybarczyk <paluho@gmail.com>
2019-03-16 10:58:18 +0100

    Add `Monoid` instance for `Distance`

--- src/Geometry/Distance.purs
+++ src/Geometry/Distance.purs
@@ -3,7 +3,6 @@ module Geometry.Distance where
 import Prelude
 
 import Data.Generic.Rep (class Generic)
-import Data.Newtype (class Newtype)
 import Geometry.Positive (NonNegative(..))
 
 foreign import kind SpaceUnit
@@ -14,10 +13,23 @@ newtype Distance (unit ∷ SpaceUnit) = Distance Number
 derive instance eqDistance ∷ Eq (Distance u)
 derive instance ordDistance ∷ Ord (Distance u)
 derive instance genericDistance ∷ Generic (Distance u) _
-derive instance newtypeDistance ∷ Newtype (Distance u) _
+
+instance semigroupDistance ∷ Semigroup (Distance u) where
+  append (Distance d1) (Distance d2) = Distance (d1 + d2)
+
+instance monoidDistance ∷ Monoid (Distance u) where
+  mempty = Distance 0.0
+
+scale ∷ ∀ u. Distance u → NonNegative → Distance u
+scale (Distance d) (NonNegative n) = Distance (d * n)
 
 unsafeScale ∷ ∀ u. Distance u → Number → Distance u
 unsafeScale (Distance d) n = Distance (d * n)
 
-scale ∷ ∀ u. Distance u → NonNegative → Distance u
-scale (Distance d) (NonNegative n) = Distance (d * n)
+ratio ∷ ∀ u. Distance u → Distance u → Number
+ratio (Distance d1) (Distance d2) = d1 / d2
+
+newtype ConversionFactor (from ∷ SpaceUnit) (to ∷ SpaceUnit) = ConversionFactor Number
+
+convert ∷ ∀ from to. ConversionFactor from to → Distance from → Distance to
+convert (ConversionFactor c) (Distance d) = Distance (c * d)
