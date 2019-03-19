689ea3b31ee3da1c322fd6da7866ac661c5a5db0
Tomasz Rybarczyk <paluho@gmail.com>
2019-03-19 17:17:02 +0100

    Add `Screen` pixel unit

--- src/Geometry/Distance/Units.purs
+++ src/Geometry/Distance/Units.purs
@@ -2,5 +2,14 @@ module Geometry.Distance.Units where
 
 foreign import kind SpaceUnit
 
+-- | Some example metrics for reuse and easy interoop
+-- | between sublibs
+
+-- | Absolute units in some space
 foreign import data Real ∷ SpaceUnit
+
+-- | Physical pixels of an image
 foreign import data Pixel ∷ SpaceUnit
+
+-- | Pixels on the screen (from click event etc.)
+foreign import data Screen ∷ SpaceUnit
