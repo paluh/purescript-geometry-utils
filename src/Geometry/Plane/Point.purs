067513079ee3e6b2f2f58498d15539fc04bc08d6
Tomasz Rybarczyk <paluho@gmail.com>
2019-06-16 18:56:41 +0200

    Drop last ffi bit

--- src/Geometry/Plane/Point.purs
+++ src/Geometry/Plane/Point.purs
@@ -13,14 +13,16 @@ import Prelude
 import Geometry.Distance (Distance, unsafeDistance)
 import Geometry.Distance (unsafeDistance) as Distance
 import Geometry.Distance.ConversionFactor (ConversionFactor(..))
+import Geometry.Matrix.Matrix2x2 (Matrix, array) as Matrix2x2
 import Geometry.Numbers.Positive (Positive(..))
 import Geometry.Plane.Point.Types (Point(..))
 import Geometry.Plane.Point.Types (_x, _y, _xD, _yD, point, Point(..)) as Types
 import Geometry.Plane.Transformations.Affine.Matrix (Matrix) as Affine
-import Geometry.Plane.Transformations.Linear.Matrix (Matrix) as Linear
+import Geometry.Plane.Transformations.Affine.Matrix (array) as Affine.Matrix
 import Geometry.Plane.Transformations.Translation (Translation(..))
 import Geometry.Plane.Vector.Types (Vector(..))
 import Math (pow, sqrt) as Math
+import Partial.Unsafe (unsafePartial)
 
 distance ∷ ∀ u. Point u → Point u → Distance u
 distance (Point { x: x1, y: y1 }) (Point { x: x2, y: y2 }) =
@@ -29,9 +31,25 @@ distance (Point { x: x1, y: y1 }) (Point { x: x2, y: y2 }) =
 distance' ∷ ∀ u. Point u → Distance u
 distance' (Point { x, y }) = Distance.unsafeDistance $ Math.sqrt (x * x + y * y)
 
-foreign import linearTransform ∷ ∀ u. Linear.Matrix → Point u → Point u
+linearTransform ∷ ∀ u. Matrix2x2.Matrix → Point u → Point u
+linearTransform m (Point { x, y }) = unsafePartial $
+  let
+    [ a11, a12, a21, a22 ] = Matrix2x2.array m
+  in
+    Point
+      { x: a11 * x + a12 * y
+      , y: a21 * x + a22 * y
+      }
 
-foreign import affineTransform ∷ ∀ u. Affine.Matrix u → Point u → Point u
+affineTransform ∷ ∀ u. Affine.Matrix u → Point u → Point u
+affineTransform m (Point { x, y }) = unsafePartial $
+  let
+    [ a11, a12, a13 , a21, a22, a23 , _, _, _ ] = Affine.Matrix.array m
+  in
+    Point
+      { x: a11 * x + a12 * y + a13
+      , y: a21 * x + a22 * y + a23
+      }
 
 translate ∷ ∀ u. Translation u → Point u → Point u
 translate (Translation (Vector { x, y })) (Point p) =
--- src/Geometry/Plane/Transformations/Affine/Matrix.purs
+++ src/Geometry/Plane/Transformations/Affine/Matrix.purs
@@ -1,5 +1,7 @@
 module Geometry.Plane.Transformations.Affine.Matrix
   ( Matrix
+  , array
+  , matrix3x3
   , unsafe
   )
   where
